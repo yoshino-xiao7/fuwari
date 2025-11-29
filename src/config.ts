a06f30a3ace95fc9101fc205e1721e80c6ab4c0e
yukiryou <xiao7@yukiryou.top>
2025-11-29 16:18:09 +0800

    更新备案域

--- src/config.ts
+++ src/config.ts
@@ -58,6 +58,7 @@ export const siteConfig: SiteConfig = {
 	],
 	officialSites: [
 		{ url: "https://blog-n.yukiryou.top", alias: "Vercel" },
+		{ url: "https://blog.yukiryou.icu", alias: "备案域" },
 		{ url: "https://blog.yukiryou.top", alias: "香港" },
 	],
 };
