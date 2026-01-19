a17eb861a4453c8514de6bc1f5ca89899dbffb59
yukiryou <xiao7@yukiryou.top>
2026-01-19 18:45:46 +0800

    更换状态页面

--- src/config.ts
+++ src/config.ts
@@ -79,7 +79,7 @@ export const navBarConfig: NavBarConfig = {
 		},
 		{
 			name: "状态",
-			url: "https://db.yukiryou.top/", // Internal links should not include the base path, as it is automatically added
+			url: "https://status.yukiryou.icu/", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 		{
